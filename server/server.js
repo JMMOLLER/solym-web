fea6b7aaf71f006f7fe0441131c4e6b4cd938cf6
Jorge M'Moller <jlmm.jlmm76@gmail.com>
2023-06-16 22:26:31 -0500

    add version API & secure operator

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api/", require("./routers/middlewares/debug") , require("./routers/API"));
-app.get("/", (req, res) => res.status(200).send("API is working successfully!!"));
+app.get("/", (req, res) => res.status(200).send("API is working successfully on v1.3.2!!"));
 
 app.listen(PORT, () => {
     logger.info(`Application listening at http://localhost:${PORT}`);
