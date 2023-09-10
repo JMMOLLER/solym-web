6b3da90b8c9bb75a30ccfefa76b105f426c31f38
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2023-09-10 00:42:02 -0500

    migration start
    - Se creo el proyecto de vite.
    - Se migró el server a otro repositorio
    - Se agregó el nuevo logo.

--- server/routers/middlewares/debug.js
+++
@@ -1,8 +0,0 @@
-const { logger } = require("../../Resources/pino");
-
-const debug = (req, res, next) => {
-    logger.debug("Request to "+req.originalUrl+" from "+req.ip+" with "+req.method+" method");
-    next();
-};
-
-module.exports = debug;
